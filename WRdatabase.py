87b3f2b426300f37bd77a538aca40272c1bc594f
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-08-14 22:17:29 -0400

    Update players structure

--- WRdatabase.py
+++ WRdatabase.py
@@ -15,87 +15,103 @@ def is_WR(pp):
         return False
 
 
-def add_player(pp):
+def add_player(pp, year, week):
+    """
+    Create a player with stats set to zero
+    """
     global players
 
-    players[str(pp.player)] = {'receiving_tar': 0,
-                               'receiving_rec': 0,
-                               'receiving_yds': 0,
-                               'receiving_tds': 0,
-                               'receiving_tds_bonus': 0,
-                               'receiving_twoptm': 0,
-                               'rushing_att': 0,
-                               'rushing_yds': 0,
-                               'rushing_tds': 0,
-                               'rushing_tds_bonus': 0,
-                               'rushing_twoptm': 0,
-                               'passing_att': 0,
-                               'passing_cmp': 0,
-                               'passing_int': 0,
-                               'passing_tds': 0,
-                               'passing_tds_bonus': 0,
-                               'passing_twoptm': 0,
-                               'fumbles_lost': 0,
-                               'fumbles_rec_tds': 0,
-                               'kickret_tds': 0,
-                               'puntret_tds': 0}
+    players[str(pp.player)] = {}
+    players[str(pp.player)][str(year)] = {}
+    players[str(pp.player)][str(year)][str(week)] = {'team': '',  # need to add method
+                                                     'opponent': '',  # need to add method
+                                                     'home_away': '',  # need to add method
+                                                     'receiving_tar': 0,
+                                                     'receiving_rec': 0,
+                                                     'receiving_yds': 0,
+                                                     'receiving_tds': 0,
+                                                     'receiving_tds_bonus': 0,
+                                                     'receiving_twopta': 0,  # need to add method
+                                                     'receiving_twoptm': 0,
+                                                     'rushing_att': 0,
+                                                     'rushing_yds': 0,
+                                                     'rushing_tds': 0,
+                                                     'rushing_tds_bonus': 0,
+                                                     'rushing_twopta': 0,  # need to add method
+                                                     'rushing_twoptm': 0,
+                                                     'passing_att': 0,
+                                                     'passing_cmp': 0,
+                                                     'passing_int': 0,
+                                                     'passing_tds': 0,
+                                                     'passing_tds_bonus': 0,
+                                                     'passing_twopta': 0,  # need to add method
+                                                     'passing_twoptm': 0,
+                                                     'fumbles_lost': 0,
+                                                     'fumbles_rec_tds': 0,
+                                                     'kickret_tds': 0,
+                                                     'puntret_tds': 0,
+                                                     'fp': 0}
 
 
-def get_wr_stats(pp):
+def get_wr_stats(pp, season, week):
     global players
-    name = str(pp.player)
+    player = players[str(pp.player)][str(season)][str(week)]
 
     # Receiving stats
-    players[name]['receiving_tar'] += pp.receiving_tar
-    players[name]['receiving_rec'] += pp.receiving_rec
-    players[name]['receiving_yds'] += pp.receiving_yds
-    players[name]['receiving_twoptm'] += pp.receiving_twoptm
+    player['receiving_tar'] += pp.receiving_tar
+    player['receiving_rec'] += pp.receiving_rec
+    player['receiving_yds'] += pp.receiving_yds
+    player['receiving_twopta'] += pp.receiving_twopta
+    player['receiving_twoptm'] += pp.receiving_twoptm
 
     if pp.receiving_tds:
         # >= 50 yards
         if pp.receiving_yds >= 50:
-            players[name]['receiving_tds_bonus'] += pp.receiving_tds
+            player['receiving_tds_bonus'] += pp.receiving_tds
         # < 50 yards
         else:
-            players[name]['receiving_tds'] += pp.receiving_tds
+            player['receiving_tds'] += pp.receiving_tds
 
     # Rushing stats
-    players[name]['rushing_att'] += pp.rushing_att
-    players[name]['rushing_yds'] += pp.rushing_yds
-    players[name]['rushing_twoptm'] += pp.rushing_twoptm
+    player['rushing_att'] += pp.rushing_att
+    player['rushing_yds'] += pp.rushing_yds
+    player['rushing_twopta'] += pp.rushing_twopta
+    player['rushing_twoptm'] += pp.rushing_twoptm
 
     if pp.rushing_tds:
         # >= 50 yards
         if pp.rushing_yds >= 50:
-            players[name]['rushing_tds_bonus'] += pp.rushing_tds
+            player['rushing_tds_bonus'] += pp.rushing_tds
         # < 50 yards
         else:
-            players[name]['rushing_tds'] += pp.rushing_tds
+            player['rushing_tds'] += pp.rushing_tds
 
     # Passing stats
-    players[name]['passing_att'] += pp.passing_att
-    players[name]['passing_cmp'] += pp.passing_cmp
-    players[name]['passing_int'] += pp.passing_int
-    players[name]['passing_twoptm'] += pp.passing_twoptm
+    player['passing_att'] += pp.passing_att
+    player['passing_cmp'] += pp.passing_cmp
+    player['passing_int'] += pp.passing_int
+    player['passing_twopta'] += pp.passing_twopta
+    player['passing_twoptm'] += pp.passing_twoptm
 
     if pp.passing_tds:
         # > 50 yards
         if pp.passing_yds >= 50:
-            players[name]['passing_tds_bonus'] += pp.passing_tds
+            player['passing_tds_bonus'] += pp.passing_tds
         else:
-            players[name]['passing_tds'] += pp.passing_tds
+            player['passing_tds'] += pp.passing_tds
 
     # Misc stats
-    players[name]['fumbles_lost'] += pp.fumbles_lost
-    players[name]['fumbles_rec_tds'] += pp.fumbles_rec_tds
-    players[name]['kickret_tds'] += pp.kickret_tds
-    players[name]['puntret_tds'] += pp.puntret_tds
+    player['fumbles_lost'] += pp.fumbles_lost
+    player['fumbles_rec_tds'] += pp.fumbles_rec_tds
+    player['kickret_tds'] += pp.kickret_tds
+    player['puntret_tds'] += pp.puntret_tds
 
 
-def get_fp(name):
+def get_fp(name, season, week):
     global players
+    player = players[name][str(season)][str(week)]
 
-    players[name]['fp'] = scoring.calc_off_fp(players[name])
+    player['fp'] = scoring.calc_off_fp(player)
 
 
 def main():
@@ -106,33 +122,34 @@ def main():
     # Conduct a query
     q = nfldb.Query(db)
 
+    seasons = range(2014, 2015)
     weeks = range(1, 2)
 
     players = {}
 
-    for week in weeks:
-        # q = nfldb.Query(db)
-        games = q.game(season_year=2014, season_type='Regular', week=week).as_games()
+    for season in seasons:
+        for week in weeks:
+            # q = nfldb.Query(db)
+            games = q.game(season_year=season, season_type='Regular', week=week).as_games()
 
-        for game in games:
-            for pp in game.play_players:
-                # If player is a WR/TE or guess_position == WR
-                if is_WR(pp):
-                    try:
-                        get_wr_stats(pp)
-                    except KeyError:
-                        add_player(pp)
-                        get_wr_stats(pp)
-
-    for player in players:
-        get_fp(player)
+            for game in games:
+                for pp in game.play_players:
+                    # If player is a WR/TE or guess_position == WR
+                    if is_WR(pp):
+                        try:
+                            get_wr_stats(pp, season, week)
+                        except KeyError:
+                            add_player(pp, season, week)
+                            get_wr_stats(pp, season, week)
+            for player in players:
+                get_fp(player, season, week)
 
 
 if __name__ == '__main__':
     main()
+    # print players
     # i = 0
     for player in players:
         # if i > 25: break
-        # get_fp(player)
-        print player, players[player]['fp']
+        print player, players[player]['2014']['1']['fp']
         # i += 1
